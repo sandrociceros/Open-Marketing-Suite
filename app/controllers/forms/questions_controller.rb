ed293f5cff21884662a85ebea60a183a0e67ec3c
NRK <hello@marketconvert.com>
2015-08-09 13:40:20 +0800

    Updated incorrect redirect path for destroy question action.

--- app/controllers/forms/questions_controller.rb
+++ app/controllers/forms/questions_controller.rb
@@ -57,7 +57,7 @@ class Forms::QuestionsController < ApplicationController
   def destroy
     @forms_question.destroy
     respond_to do |format|
-      format.html { redirect_to forms_questions_url, notice: 'Question was successfully destroyed.' }
+      format.html { redirect_to forms_set_questions_path, notice: 'Question was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
